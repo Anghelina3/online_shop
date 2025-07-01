af484251c8d6c214205603b60b62e5eb368356fd
Anghelina <angelinakonina67@gmail.com>
2025-07-01 18:16:28 +0300

    add version with spring boot
    # Conflicts:
    #	.gitignore
    #	pom.xml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module MAGAZINE {
        requires javafx.controls;
        requires java.sql;
        requires java.desktop;
+       requires jakarta.persistence;
+       requires static lombok;
        exports usersAPP;
 
 }
